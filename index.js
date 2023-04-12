623fa80f6f6207d1f4b5d278c0448554952e5c7f
gustavolara11 <gustavex2007@gmail.com>
2023-04-12 12:54:38 -0300

    feat: fix dot issues

--- index.js
+++ index.js
@@ -4,6 +4,56 @@ function visor(num) {
   resultado = resultado + num;
   document.getElementById("resultado").value = resultado;
 }
+function visor1(num) {
+  if (!resultado.includes(".")) {
+    resultado = resultado + num;
+    document.getElementById("resultado").value = resultado;
+  }
+  if (resultado.includes("+")) {
+    let opP = "+";
+    let dot1 = resultado.split(opP, 2);
+
+    if (resultado.includes("+")) {
+      if (!dot1[1].includes(".")) {
+        resultado = resultado + num;
+        document.getElementById("resultado").value = resultado;
+      }
+    }
+  }
+  if (resultado.includes("-")) {
+    let opS = "-";
+    let dot2 = resultado.split(opS, 2);
+
+    if (resultado.includes("-")) {
+      if (!dot2[1].includes(".")) {
+        resultado = resultado + num;
+        document.getElementById("resultado").value = resultado;
+      }
+    }
+  }
+  if (resultado.includes("/")) {
+    let opD = "/";
+    let dot3 = resultado.split(opD, 2);
+
+    if (resultado.includes("/")) {
+      if (!dot3[1].includes(".")) {
+        resultado = resultado + num;
+        document.getElementById("resultado").value = resultado;
+      }
+    }
+  }
+  if (resultado.includes("*")) {
+    let opM = "*";
+    let dot4 = resultado.split(opM, 2);
+
+    if (resultado.includes("*")) {
+      if (!dot4[1].includes(".")) {
+        resultado = resultado + num;
+        document.getElementById("resultado").value = resultado;
+      }
+    }
+  }
+}
 
 function ac() {
   resultado = "";
